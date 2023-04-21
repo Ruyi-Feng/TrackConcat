06d55048bd5b804e8321e9af94dd87a1de8aa827
Ruyi-Feng <fengruyi@seu.edu.cn>
2023-04-21 22:09:44 +0800

    feat: Added concat module

---
+++ concat/concat.py
@@ -0,0 +1,109 @@
+from nstransformer.model.exp_test import Exp_Test
+from siamvgg.model.exp_siam import Exp_Siam
+import pandas as pd
+import copy
+import cv2
+import torch
+
+class Trackconcat():
+    """ func
+    1. 按照时序，历史数据段48-96，预测96。96范围内所有candi_id存成dict。
+    2. 每个break 包含：history，predict，imgfeature(siamvgg的对象)， 他的candidates
+    3. 每个candi的属性：id, distance, similarity, adjust_dis
+
+    breaks: dict
+    ---
+    {
+        break_id: {
+            "lane": 0,
+            "history": {"date": [], "speed": []},
+            "predict": [],
+            "imagefeature": obj,
+            "candidates": {
+                candi_1: {"distance": 10, "similarity": 0.5, "adjust_dis": 5},
+                candi_2: {"distance": 12, "similarity": 0.2, "adjust_dis": 9},}
+            "select" : candi_1
+        },
+        break_id: {...},
+    }
+
+    根据break list id 来初始化breaks dict
+    nstransformer去predict，提取obj
+    找candidate， generate
+    """
+    label_dict = {"HeadwaySpeed": ["date", "headway", "speed", "longitude"],
+                "HeadwaySpeedAcc": ["date", "headway", "speed", "acceleration", "longitude"],
+                "TTCSpeed": ["date", "ttc", "speed", "longitude"],
+                "TTCSpeedAcc": ["date", "ttc", "speed", "acceleration", "longitude"],
+                "withAcc": ["date", "acceleration", "longitude"],
+                "withHeadway": ["date", "headway", "longitude"],
+                "withLeaderPos": ["date", "leaderpos", "longitude"],
+                "withSpeed": ["date", "speed", "longitude"],
+                "withTTC": ["date", "ttc", "longitude"],}
+
+    def __init__(self, args, params, dir, flnm):
+        self.siam = Exp_Siam(params)
+        self.nstrans = Exp_Test(args)
+        self.dir = dir
+        self.flnm = dir + "\\" + flnm
+        self.seq_len = args.seq_len
+        self.label = args.label
+
+    def _init_breaks(self, frame, break_list):
+        """
+        用于读取breaklist中所用目标
+        在frame之前96长度的序列
+        初始化self.breaks并放在history里
+        """
+        data = pd.read_csv(self.flnm)
+        data = data.sort_values(by=["frame"], ascending=True)
+        data = data.reset_index(drop=True)
+        pre = data.iloc[(data["frame"] <= frame) & (data["frame"] >= frame - self.seq_len)]
+        self.post = data.iloc[(data["frame"] > frame) & (data["frame"] <= frame + self.seq_len)]
+        for id in break_list:
+            tmp = pre.iloc[pre["car_id"] == id]
+            lane = tmp["lane"][-1]
+            tmp = tmp[self.label_dict[self.label]]
+            tmp_dict = copy.deepcopy(tmp.to_dict(orient = 'list'))
+            self.breaks.update({id: {"lane": lane, "history": {}, "predict": [], "candidates": {}, "select": -1}})
+            self.breaks["history"].update(tmp_dict)
+        self.breaks
+
+    def _dis(self, ):
+        pass
+
+    def _candi(self, frame, output, lane):
+        candidates = dict()
+        group = self.post.iloc[self.post.iloc["lane"] == lane]
+        # 目前group里包含的是seq96的lane = lane的所有车辆轨迹
+        # 接下来直接找longitude，在frame中相距小于车长的。
+        # index = group["frame"][i] - frame
+        # group[now_frame]
+        pass
+
+
+
+    def concat(self, frame, break_list):
+        # 在第frame帧断了的id存在break_list中
+        self.breaks = dict()
+        self._init_breaks(frame, break_list)
+        for key in self.breaks:
+            # --- generate prediction ---
+            input = pd.DataFrame(self.breaks[key]["history"])
+            output = self.nstrans.test(input)
+            torch.cuda.empty_cache()
+            self.breaks[key]["predict"] = output.tolist()
+
+            # --- generate image feature ---
+            img_refer = cv2.imread(self.dir+"//%d.jpg"%key)
+            self.siam.define_refer(img_refer)
+
+            # --- generate candidate ---
+            candidates, select = self._candi(frame, output, self.breaks[key]["lane"])
+            self.breaks[key]["candidates"].update(candidates)
+            self.breaks[key]["select"] = select
+
+        return self.breaks
+
+
+
