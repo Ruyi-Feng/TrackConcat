d119c6b5a3a327f7e31b325b46483a05c22a1946
Ruyi-Feng <fengruyi@seu.edu.cn>
2023-04-25 21:02:20 +0800

    feat: Added main

---
+++ main.py
@@ -0,0 +1,15 @@
+from nstransformer.utils.param import param
+from concat.exp_concat import Exp_concat
+
+
+if __name__ == '__main__':
+    label = "TTCSpeedAcc"
+    dir = ".\\data\\img\\RML7"
+    args = param(label, "MS", "", 4)
+    flnm = "packed.csv"
+    params = {"model_path": "./weights/VGG16/Epoch99.pth", "input_shape": (60, 120, 3)}
+
+    concat = Exp_concat(args, params, dir, flnm)
+    concat.run()
+    print("done!")
+
